b0a76f8f3390fe608146685759d489738512bbab
aldreysilva <94716967+aldreysilva@users.noreply.github.com>
2023-01-06 14:43:20 -0300

    renderizando
    renderizando

--- zeldaminiclone/src/zeldaminiclone/Game.java
+++ zeldaminiclone/src/zeldaminiclone/Game.java
@@ -1,7 +1,10 @@
 package zeldaminiclone;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
 
@@ -15,6 +18,36 @@ public class Game extends Canvas implements Runnable{
 		this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		
 	}
+	
+	
+	public void tick() {
+		
+	}
+	
+	public void render() {
+		//começo render automatico 
+		BufferStrategy bs = this.getBufferStrategy();
+			if(bs == null) {
+				this.createBufferStrategy(3);
+				return;
+			}
+		Graphics g = bs.getDrawGraphics();
+		
+		
+		//fundo
+		g.setColor(Color.black);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		
+		//redanculo
+		g.setColor(Color.yellow);
+		g.fillRect(10, 10, 50, 50);
+		
+		
+		bs.show();
+		//fim render automatico 
+	}
+	
+	
 	public static void main(String[] args) {
 		Game game = new Game();
 		JFrame frame = new JFrame();
@@ -22,21 +55,36 @@ public class Game extends Canvas implements Runnable{
 		frame.add(game);
 		//titulo da tela
 		frame.setTitle("Mini Zelda");
-		//centraliza a tela
-		frame.setLocationRelativeTo(null);
+		
 		//calcula o tamanho correto da janela
 		frame.pack();
+		
+		//centraliza a tela
+		frame.setLocationRelativeTo(null);
 		
 		//fecha a janela
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//visualizar a janela
 		frame.setVisible(true);
 		
+		
+		new  Thread(game).start(); //começar o jogo 
+		
 	}
 
+	
 	@Override
 	public void run() {
 		
+		while(true) {
+			tick();
+			render();
+			try {
+				Thread.sleep((1000/60));
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 		
 
 	}
